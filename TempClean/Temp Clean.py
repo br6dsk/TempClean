a9e8e8a24217bbe3d2ea637d0f2fd8047a69651e
brndsk <44901467+brndsk@users.noreply.github.com>
2020-01-24 13:19:02 -0300

    Update Temp Clean.py

--- TempClean/Temp Clean.py
+++ TempClean/Temp Clean.py
@@ -1,4 +1,4 @@
-#Author: Braian D. Jesus
+#Author: brndsk
 #Este programa exclui pra você todos os arquivos temporários do seu Windows!
 #Caso o programa não funcione corretamente, modifique os arquivos de Lote (.bat) com os seus próprios diretórios.
 #Usando windows+r, digita o %temp% da enter e copia o caminho da barra de endereços e mude no arquivo file_tt.bat.
@@ -40,28 +40,28 @@ if pergunta == 1:
 	os.system("start file_t.bat")
 	sleep(0.3)
 	print(" \n\t\t\tTemp apagado!")
-	print(" \n\t\t\tObrigado por usar! by Braian D. Jesus.")
+	print(" \n\t\t\tObrigado por usar! by Brndsk.")
 elif pergunta ==2:
 	sleep(0.3)
 	print(" \n\t\t\tApagando...")
 	os.system("start file_p.bat")
 	sleep(0.3)
 	print(" \n\t\t\tPrefetch apagado!")
-	print(" \n\t\t\tObrigado por usar! by Braian D. Jesus.")
+	print(" \n\t\t\tObrigado por usar! by Brndsk.")
 elif pergunta ==3:
 	sleep(0.3)
 	print(" \n\t\t\tApagando...")
 	os.system("start file_tt.bat")
 	sleep(0.3)
 	print(" \n\t\t\t%Temp% apagado!")
-	print(" \n\t\t\tObrigado por usar! by Braian D. Jesus.")
+	print(" \n\t\t\tObrigado por usar! by Brndsk.")
 elif pergunta ==4:
 	sleep(0.3)
 	print(" \n\t\t\tApagando...")
 	os.system("start file_d.bat")
 	sleep(0.3)
 	print(" \n\t\t\tLiberação do Cache do DNS Resolver bem-sucedida.")
-	print(" \n\t\t\tObrigado por usar! by Braian D. Jesus.")
+	print(" \n\t\t\tObrigado por usar! by Brndsk.")
 elif pergunta ==5:
 	sleep(0.3)
 	print(" \n\t\t\tApagando...")
@@ -72,9 +72,9 @@ elif pergunta ==5:
 	sleep(0.3)
 	print(" \n\t\t\tA maioria dos arquivos temporários do sistema, foram apagados!")
 	print(" \n\t\t\tLiberação do Cache do DNS Resolver bem-sucedida.")
-	print(" \n\t\t\tObrigado por usar! by Braian D. Jesus.")	
+	print(" \n\t\t\tObrigado por usar! by Brndsk.")	
 elif pergunta==0:
-	print(" \n\t\t\tObrigado por usar! by Braian D. Jesus.")
+	print(" \n\t\t\tObrigado por usar! by Brndsk.")
 	quit()
 else:
 	print("\n\t\t\tNumero inválido")
